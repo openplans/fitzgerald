a7c0bb340707fd19826c3daefae743b0e9d7a3a0
Aaron Ogle <atogle@gmail.com>
2012-06-20 12:38:54 -0400

    add tooltip view

--- js/views.js
+++ js/views.js
@@ -1,11 +1,29 @@
 var Fitzgerald = Fitzgerald || {};
 
 (function(F) {
+  // Setup the namespace to trigger/bind events
+  _.extend(F, Backbone.Events);
+
   // F.View = Backbone.View.extend({
   //   el: '',
   //   initialize: function(){},
   //   render: function(){}
   // });
+
+
+  F.TooltipView = Backbone.View.extend({
+    el: '.dot-tooltip',
+    initialize: function(){
+      F.on('locationupdate', this.render, this);
+    },
+    render: function(index, percent){
+      var self = this;
+
+      $(self.el)
+        .css('left', (percent*100) + '%')
+        .html('<strong>' + self.model.at(index).get('feedback').length + '</strong> Comments');
+    }
+  });
 
   // The map slider view
   F.NavigatorView = Backbone.View.extend({
@@ -20,9 +38,10 @@ var Fitzgerald = Fitzgerald || {};
       $(self.el).slider({
         max: self.model.length,
         slide: function(evt, ui) {
-          $(F).trigger('updatelocation', [ui.value, ui.value/self.model.length]);
+          F.trigger('locationupdate', ui.value, ui.value/self.model.length);
         },
         stop: function(evt, ui) {
+          // Update the cursor icon
           $(ui.handle).removeClass('grabbed');
         }
       });
@@ -31,6 +50,9 @@ var Fitzgerald = Fitzgerald || {};
       $('.ui-slider-handle', self.el).mousedown(function(){
         $(this).addClass('grabbed');
       });
+
+      // Update location to the first intersection
+      F.trigger('locationupdate', 0, 0);
     }
   });
 
@@ -40,9 +62,8 @@ var Fitzgerald = Fitzgerald || {};
       this.model = new F.IntersectionCollection();
 
       // The map slider
-      this.mapSliderView = new F.NavigatorView({
-        model: this.model
-      });
+      this.mapSliderView = new F.NavigatorView({ model: this.model });
+      this.tooltipView = new F.TooltipView({ model: this.model });
 
       // Fetch the intersection records
       this.model.fetch();
