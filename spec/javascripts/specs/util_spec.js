780de933cc3cb7513b11f6aa3ffaa8f3c7f5c2dc
Aaron Ogle <atogle@gmail.com>
2012-06-21 16:05:14 -0400

    fix a bug in Util.isLocalhost

--- spec/javascripts/specs/util_spec.js
+++ spec/javascripts/specs/util_spec.js
@@ -3,6 +3,7 @@
     it('checks if a url is a localhost address', function() {
       expect(F.Util.isLocalhost('http://localhost:8888/blah')).toBe(true);
       expect(F.Util.isLocalhost('http://localhost/')).toBe(true);
+      expect(F.Util.isLocalhost('localhost')).toBe(true);
       expect(F.Util.isLocalhost('http://127.0.0.1/')).toBe(true);
       expect(F.Util.isLocalhost('http://openplans.org')).toBe(false);
     });
--- src/js/util.js
+++ src/js/util.js
@@ -5,7 +5,7 @@ var Fitzgerald = Fitzgerald || {};
   F.Util = {
     isLocalhost: function(url){
       url = url || location.hostname;
-      return (url.indexOf('localhost') > 0 || url.indexOf('127.0.0.1') > 0);
+      return (url.indexOf('localhost') >= 0 || url.indexOf('127.0.0.1') >= 0);
     }
   };
 })(Fitzgerald, jQuery);
